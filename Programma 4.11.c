bc3a9785116c6a0eadb085e6c781406f71d5fe96
Alle Cross Costi <costialle2@hotmail.it>
2018-02-03 16:25:39 +0100

    Initial commit

---
+++ Programma 4.11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+
+ int main () {
+ 
+ int intNUM1, intNUM2;
+ 
+ printf ("Digita il primo numero: ");
+ scanf ("%d", &intNUM1);
+ printf ("Digita il secondo numero: ");
+ scanf ("%d", &intNUM2);
+ 
+ printf ("\nN.1: %d\nN.2: %d", intNUM1, intNUM2);
+ 
+ return 0;
+ 
+ }
